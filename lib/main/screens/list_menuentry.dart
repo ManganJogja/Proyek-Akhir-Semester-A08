b720f294af0935d30f256b489b929809a53e2b62
ridyaazizahkm <ridyazizahh3@gmail.com>
2024-12-02 13:35:35 +0700

    tambah buttom navbar dan rapihin drawer

---
+++ lib/main/screens/list_menuentry.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:mangan_jogja/models/menu_entry.dart';
+import 'package:mangan_jogja/widgets/drawer.dart';
+import 'package:mangan_jogja/widgets/bottom_navbar.dart';
---
+++ lib/models/menu_entry.dart
@@ -0,0 +1,81 @@
+// To parse this JSON data, do
+//
+//     final menuEntry = menuEntryFromJson(jsonString);
+
+import 'dart:convert';
+
+List<MenuEntry> menuEntryFromJson(String str) => List<MenuEntry>.from(json.decode(str).map((x) => MenuEntry.fromJson(x)));
+
+String menuEntryToJson(List<MenuEntry> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class MenuEntry {
+    Model model;
+    String pk;
+    Fields fields;
+
+    MenuEntry({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
+
+    factory MenuEntry.fromJson(Map<String, dynamic> json) => MenuEntry(
+        model: modelValues.map[json["model"]]!,
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "model": modelValues.reverse[model],
+        "pk": pk,
+        "fields": fields.toJson(),
+    };
+}
+
+class Fields {
+    String namaMenu;
+    String deskripsi;
+    String imageUrl;
+    List<String> restaurants;
+
+    Fields({
+        required this.namaMenu,
+        required this.deskripsi,
+        required this.imageUrl,
+        required this.restaurants,
+    });
+
+    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        namaMenu: json["nama_menu"],
+        deskripsi: json["deskripsi"],
+        imageUrl: json["image_url"],
+        restaurants: List<String>.from(json["restaurants"].map((x) => x)),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "nama_menu": namaMenu,
+        "deskripsi": deskripsi,
+        "image_url": imageUrl,
+        "restaurants": List<dynamic>.from(restaurants.map((x) => x)),
+    };
+}
+
+enum Model {
+    ADMIN_DASHBOARD_MENUENTRY
+}
+
+final modelValues = EnumValues({
+    "admin_dashboard.menuentry": Model.ADMIN_DASHBOARD_MENUENTRY
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    late Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+            reverseMap = map.map((k, v) => MapEntry(v, k));
+            return reverseMap;
+    }
+}
